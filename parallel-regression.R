d2710294b8952c540d6e194de97a8a08f8c5e2ac
muh riansyah <mriansyah93@gmail.com>
2019-04-06 16:57:36 +0700

    add regression logistic

---
+++ parallel-regression.R
@@ -0,0 +1,18 @@
+# run in parallel mode
+library(parallel)
+library(MASS)
+
+numCores <- detectCores()
+
+
+x <- iris[which(iris[,5] != "setosa"), c(1,5)]
+trials <- seq(1, 10000)
+boot_fx <- function(trial) {
+  ind <- sample(100, 100, replace=TRUE)
+  result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
+  r <- coefficients(result1)
+  res <- rbind(data.frame(), r)
+}
+system.time({
+  results <- mclapply(trials, boot_fx, mc.cores = numCores)
+})
---
+++ seq-regression.R
@@ -0,0 +1,13 @@
+x <- iris[which(iris[,5] != "setosa"), c(1,5)]
+trials <- 10000
+res <- data.frame()
+system.time({
+  trial <- 1
+  while(trial <= trials) {
+    ind <- sample(100, 100, replace=TRUE)
+    result1 <- glm(x[ind,2]~x[ind,1], family=binomial(logit))
+    r <- coefficients(result1)
+    res <- rbind(res, r)
+    trial <- trial + 1
+  }
+})
